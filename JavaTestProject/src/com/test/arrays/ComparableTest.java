9df7a2413087578bd5fe6b96c10a360ce2901f99
Steve <steveal@126.com>
2015-10-28 10:16:28 +0800

    add some change

--- JavaTestProject/src/com/test/arrays/ComparableTest.java
+++ JavaTestProject/src/com/test/arrays/ComparableTest.java
@@ -13,14 +13,19 @@ public class ComparableTest {
 		// TODO Auto-generated method stub
 		int i = 2;
 		int j = 3;
-		System.out.println(compare(i,j,null));
+		System.out.println(compare(i,j));
 		String a = "a";
 		String b = "b";
-		System.out.println(compare(a,b,null));
-		System.out.println(compare(b,b,null));
-		System.out.println(compare(b,a,null));
+		System.out.println(compare(a,b));
+		System.out.println(compare(b,b));
+		System.out.println(compare(b,a));
 		
 
+	}
+	
+	public static int compare(Object i,Object j) {
+		return compare(i,j,null);
+		
 	}
 	
 	public static int compare(Object i,Object j,Comparator<?> comparator) {
@@ -38,13 +43,13 @@ public class ComparableTest {
 	public void unComparableClass() {
 		Person p1 = new Person("p1");
 		Person p2 = new Person("p2");
-		System.out.println(compare(p1,p2,null));
+		System.out.println(compare(p1,p2));
 	}
 	
 	@Test
 	public void comparableClass() {
 		Employee staff1 = new Employee(1,"staff1",Position.Staff);
 		Employee staff2 = new Employee(2,"staff2",Position.Staff);
-		Assert.assertTrue(compare(staff1,staff2,null)<0);
+		Assert.assertTrue(compare(staff1,staff2)<0);
 	}
 }
