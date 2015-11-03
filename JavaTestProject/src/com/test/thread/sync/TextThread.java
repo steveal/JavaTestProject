da77a92cdca9b4f4b0c43c09f7504c7f79b8da20
Steve <steveal@126.com>
2015-11-03 18:28:23 +0800

    add sync example

---
+++ JavaTestProject/src/com/test/thread/sync/TextThread.java
@@ -0,0 +1,31 @@
+package com.test.thread.sync;
+
+public class TextThread {
+
+	public static void main(String[] args) {
+		TxtThread tt = new TxtThread();
+		new Thread(tt,"thread 1").start();
+		new Thread(tt,"thread 2").start();
+		new Thread(tt,"thread 3").start();
+		new Thread(tt,"thread 4").start();
+	}
+}
+
+class TxtThread implements Runnable {
+	int num = 10;
+	String str = new String();
+
+	public void run() {
+		synchronized (str) {
+			while (num > 0) {
+
+				try {
+					Thread.sleep(1);
+				} catch (Exception e) {
+					e.getMessage();
+				}
+				System.out.println(Thread.currentThread().getName() + " this is " + num--);
+			}
+		}
+	}
+}
