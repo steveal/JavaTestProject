9852892a341577595fdc6ad6b7dfebed60d65d5f
steveal <steveal@126.com>
2017-07-18 09:38:19 +0800

    redis test

--- src/main/java/com/test/jedis/pushpop/RedisLPUSH.java
+++ src/main/java/com/test/jedis/pushpop/RedisLPUSH.java
@@ -7,6 +7,7 @@ public class RedisLPUSH {
 	public static void main(String[] args) {
 		Jedis jRedis = new Jedis("10.45.80.26",6379);
 		for(int i = 0; i< 50 ; i++) {
+			
 			try {
 				Thread.sleep(1000);
 				jRedis.lpush("redistest", "msg:" + i);
